2f1d0beb1b9f129130946d149e6a04cd1034e0e1
Thiago Krüger <thiagork@msn.com>
2019-01-27 21:55:42 +0100

    Added some functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-alert('Hello world');
+var repository = [
+    {name: "Pikachu", height: 0.4, types: ['electric']}, 
+    {name: "Charmander", height: 0.6, types: ['fire']},
+    {name: "Magikarp", height: '0.9', types: ['water']}];
+
+
+for (i=0; i < repository.length; ++i) {
+    document.write(repository[i].name + " (height: " + repository[i].height + ")");
+    if (repository[i].height > 0.7) {
+        document.write(" - Wow, that's big!");
+    }
+    document.write("<BR>" + "<BR>");
+
+}
