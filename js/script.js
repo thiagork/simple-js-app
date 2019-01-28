eaeabec54d7594fbe0d9ecc0aaf8a0bfdfc03452
Thiago Krüger <thiagork@msn.com>
2019-01-28 21:12:40 +0100

    Wrapped code in IIFE, added functions getAll and add, replaced for loop with forEach

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
-var repository = [
-    {name: "Pikachu", height: 0.4, types: ["electric"]}, 
-    {name: "Charmander", height: 0.6, types: ["fire"]},
-    {name: "Magikarp", height: '0.9', types: ["water"]}];
+var pokemonRepository = (function () {
+    var repository = [
+        {name: "Pikachu", height: 0.4, types: ["electric"]}, 
+        {name: "Charmander", height: 0.6, types: ["fire"]},
+        {name: "Magikarp", height: '0.9', types: ["water"]}];
+    
+
+    function getAll () {
+        return repository;
+    }
 
 
-for (i=0; i < repository.length; ++i) {
-    document.write(repository[i].name + " (height: " + repository[i].height + ")");
-    if (repository[i].height > 0.7) {
+    function add (pokemon) {
+        repository.push(pokemon);
+    }    
+
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+}) ();
+
+
+pokemonRepository.getAll().forEach(element => {
+    document.write(element.name + " (height: " + element.height + ")");
+    if (element.height > 0.7) {
         document.write(" - Wow, that's big!");
     }
     document.write("<BR>" + "<BR>");
-}
+});
