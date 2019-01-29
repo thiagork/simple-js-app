38af635d65da1ca82a2cae91cb37df516a1a7cff
Thiago Krüger <thiagork@msn.com>
2019-01-29 22:12:34 +0100

    Added HTML, CSS. Added new addListItem() and ShowDetails() functions.

--- js/script.js
+++ js/script.js
@@ -15,17 +15,49 @@ var pokemonRepository = (function () {
     }    
 
 
+    function addListItem (pokemonName) {
+        // Creates and appends the list item
+        var $newListItem = document.createElement("li");
+        var $appendNewListItem = document.querySelector(".pokemon-list");
+        $newListItem.setAttribute("class", "pokemon-list__item");
+        $appendNewListItem.appendChild($newListItem);
+
+        // Creates and appends the button to the newly created list item
+        var $newButtonInsideListItem = document.createElement("button");
+        $newButtonInsideListItem.setAttribute("class", "pokemon-list__item__button");
+        var $appendNewButtonInsideListItem = document.querySelector(".pokemon-list__item:last-child");
+        $newButtonInsideListItem.innerText = pokemonName;
+        $appendNewButtonInsideListItem.appendChild($newButtonInsideListItem);
+
+        // Adds event listener to the list item
+        $newButtonInsideListItem.addEventListener("click", element =>{
+            var $clickedButton = element.target;
+            showDetails($clickedButton.innerText)
+        })
+    }
+
+
+    function showDetails(pokemonName) {
+        
+        var pokemon = repository.filter(element => {
+            return element.name === pokemonName;
+        });
+        pokemon.map(element => {
+            console.log(element);
+        });
+    }
+
+
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails
     };
 }) ();
 
 
 pokemonRepository.getAll().forEach(element => {
-    document.write(element.name + " (height: " + element.height + ")");
-    if (element.height > 0.7) {
-        document.write(" - Wow, that's big!");
-    }
-    document.write("<BR>" + "<BR>");
+    // Shows every pokemon inside repository in the app
+    pokemonRepository.addListItem(element.name);
 });
