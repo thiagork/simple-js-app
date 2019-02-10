a46f7df4eedb1ce411d67bd5ddf4a3ca139e9d44
Thiago Krüger <thiagork@msn.com>
2019-02-10 15:37:37 +0100

    Changed all the functions using promises to callbacks just for practice

--- js/script.js
+++ js/script.js
@@ -1,91 +1,104 @@
-var programWrapper = (function () {
-    var repository = [];
-    var apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
-    
-    function getAll () {
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+let programWrapper = (() => {
+    let repository = [];
+
+    function getAll() {
         return repository;
     }
     
-    
-    function add (item) {
+    function add(item) {
         repository.push(item);
     }    
     
-    function addListItem (item, index) {
+    function addListItem(item, index) {
         // Creates and appends the list item
-        var $newListItem = document.createElement("li");
-        var $appendNewListItem = document.querySelector(".item-list");
-        $newListItem.setAttribute("class", "item-list__item");
+        let $newListItem = document.createElement('li');
+        let $appendNewListItem = document.querySelector('.item-list');
+        $newListItem.setAttribute('class', 'item-list__item');
         $appendNewListItem.appendChild($newListItem);
         
         // Creates and appends the button to the newly created list item
-        var $newButtonInsideListItem = document.createElement("button");
-        $newButtonInsideListItem.setAttribute("class", "item-list__item__button");
-        $newButtonInsideListItem.setAttribute("id", String(index));
-        var $appendNewButtonInsideListItem = document.querySelector(".item-list__item:last-child");
-        $newButtonInsideListItem.innerText = item["name"];
+        let $newButtonInsideListItem = document.createElement('button');
+        $newButtonInsideListItem.setAttribute('class', 'item-list__item__button');
+        $newButtonInsideListItem.setAttribute('id', String(index));
+        let $appendNewButtonInsideListItem = document.querySelector('.item-list__item:last-child');
+        $newButtonInsideListItem.innerText = item['name'];
         $appendNewButtonInsideListItem.appendChild($newButtonInsideListItem);
         
         // Adds event listener to the list item
-        $newButtonInsideListItem.addEventListener("click", element =>{
-            var $clickedButton = element.target;
+        $newButtonInsideListItem.addEventListener('click', element =>{
+            let $clickedButton = element.target;
             showDetails($clickedButton.id);
         });
     }
     
-    function loadList() {
-        return fetch(apiUrl).then(function (response) {
-            return response.json();
-        }).then(function (json) {
-            json.results.forEach(function (item) {
-                var data = {
-                    name: item.name,
-                    detailsUrl: item.url
-                };
-                add(data);
-            });
-        }).catch(function (e) {
-            console.error(e);
+    function makeRequest(url, callback) {
+        let request = new XMLHttpRequest();
+    
+        request.addEventListener('load', resolve);
+        request.addEventListener('error', reject);
+    
+        request.open('GET', url);
+        request.send();
+    
+
+        function reject() {
+            console.log('The request failed (maybe you are offline?)');
+        }
+
+        function resolve(e) {
+            let xhr = e.target;
+            callback(xhr.responseText);
+        }
+    }
+
+    function loadList(responseFromAPI) {
+        // Gets only Name and URL from the API
+        JSON.parse(responseFromAPI).results.forEach(item => {
+            let data = {
+                name: item.name,
+                detailsUrl: item.url
+            };
+            // Adds the retrieved data to the Repository
+            add(data);
         });
-    }    
-    
-    
-    function loadDetails(item) {
-        var url = item.detailsUrl;
-        return fetch(url).then(function (response) {
-            return response.json();
-        }).then(function (details) {
-            // Now we add the details to the item
-            item.imageUrl = details.sprites.front_default;
-            item.height = details.height;
-            item.types = Object.keys(details.types);
-        }).catch(function (e) {
-            console.error(e);
+        
+        // Populates the DOM with the loaded list
+        getAll().forEach((item, index) => {
+            addListItem(item, index);
         });
     }
+
+    function loadDetails(responseFromAPI) {
+        let details = JSON.parse(responseFromAPI);
+        // id is the same as position in array(index) + 1
+        let item = getAll()[details.id - 1];
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = Object.keys(details.types);
+        console.log(item);
+    }
     
-    
+
     function showDetails(item) {
-        programWrapper.loadDetails(repository[item]).then(function () {
-            console.log(repository[item]);   
-        });
+        var requestUrl = getAll()[item].detailsUrl;
+        makeRequest(requestUrl, loadDetails);
+        // programWrapper.loadDetails(getAll()[item].detailsUrl), (() => {
+        //     console.log(repository[item]);   
+        // });
     }
-    
-    
+
     return {
         add: add,
         addListItem: addListItem,
         getAll: getAll,
-        // search: search,
         loadList: loadList,
         loadDetails: loadDetails,
-        showDetails: showDetails
+        showDetails: showDetails,
+        makeRequest: makeRequest
     };
 }) ();
 
 
-programWrapper.loadList().then(function() {
-    // Now the data is loaded!
-    programWrapper.getAll().forEach(function(item, index){
-        programWrapper.addListItem(item, index);
-    });});
+programWrapper.makeRequest(apiUrl, programWrapper.loadList);
